b3ac3c267969c923452d3232ec72ee3301c70543
serafinlc03022002 <48559376+serafinlc03022002@users.noreply.github.com>
2019-05-21 19:55:47 -0500

    5

--- js/acciones.js
+++ js/acciones.js
@@ -1,36 +1,32 @@
 //JavaScript Document
 
 $(document).ready(function(e){
-	document.addEventListener ("deviceready",onDeviceReady,false);
+	document.addEventListener("deviceready", onDeviceReady, false);
 	
 });
 
-    function onDeviceReady(){
-		
-		$('#posicion').on('click',function(){
-			getPosition();
-		});
+function onDeviceReady(){
+	
+	$('#pocision').on('click',function(){
+		getPosition ();
+	});
+}
+
+function getPosition(){
+	var options={
+		enableHighAccuracy : true,
+		maximunAge: 3600000
 	}
 	
-	function getPosition(){
-		var options={
-			enableHighAccuracy :true,
-			maximumAge:3600000
-		}
+	var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	
+	function onSuccess(position){
 		
-		var watchID = navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
-		
-		function onSuccess(position){
-			
-			alert('Latitude:'   + position.coords.latitude    +'\n'+
-			  'Longitude:'      + position.coords.longitude   +'\n');
-		};
-		
-		function onError(error){
-			alert('code:'   + error.code   +'\n'+'message:'+error.message +'\n');
-		}
+		alert('Latitude:'   +position.coords.latitude  +'\n'+
+		'Longitude: '  +position.coords.longitude  +'\n');
+	};
+	
+	function onError(error){
+		alert('code:'  +error.code  +'\n' + 'message:' + error.message + '\n');
 	}
-		
-		
-		
-		
+}			
